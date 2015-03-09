8d4f5265b5646ce59d6c336573371cf9f772a71f
Jacy Wang <jacywj@gmail.com>
2015-03-08 22:01:07 -0400

    Initialize repo and add layout

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue: Int = 0
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showAlert() {
+        let message = "The value of the slider is : \(currentValue)"
+        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func slideMoved(slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
 }
 

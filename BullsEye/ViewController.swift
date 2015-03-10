f3f47d4003111b5bc7df4da54aae9e668eb6018a
Jacy Wang <jacywj@gmail.com>
2015-03-09 22:11:07 -0400

    Connect labels

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,13 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var currentValue: Int = 0
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        currentValue = lroundf(slider.value)
+        startNewRound()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,15 +32,34 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-        let message = "The value of the slider is : \(currentValue)"
+        let difference: Int = abs(currentValue - targetValue)
+        let points = 100 - difference
+        score += points
+        
+        let message = "You scored \(points) points"
         let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .Alert)
         let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
         alert.addAction(action)
         presentViewController(alert, animated: true, completion: nil)
+        startNewRound()
+        updateLabels()
     }
     
     @IBAction func slideMoved(slider: UISlider) {
         currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        round += 1
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
 }
 

04c8ba175be4de3c9fb96ed9b9ebd591638585e3
Jacy Wang <jacywj@gmail.com>
2015-03-13 22:43:48 -0400

    Finish

---
+++ BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by JIAN WANG on 3/10/15.
+//  Copyright (c) 2015 JACY WANG. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        if let htmlFile = NSBundle.mainBundle().pathForResource("BullsEye", ofType: "html") {
+            let htmlData = NSData(contentsOfFile: htmlFile)
+            let baseURL = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath)
+            webView.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
